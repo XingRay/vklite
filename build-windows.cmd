663ff30d4cdb56a837d24ff35fc598e6af7df325
XingRay <leixing1012@gmail.com>
2025-08-18 19:09:41 +0800

    1 更新编译脚本

--- build-windows.cmd
+++ build-windows.cmd
@@ -2,5 +2,7 @@
 cmake -S . -B build -G "Visual Studio 17 2022" -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=build/dist
 
 cmake --build ./build --config Release
-
 cmake --install ./build --config Release
+
+cmake --build ./build --config Debug
+cmake --install ./build --config Debug
